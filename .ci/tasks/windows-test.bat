4cf43f76bd220d9255e84f6c0085050cfa2ea7c1
Ellie <hi@ellie.moe>
2018-10-13 20:34:18 +0200

    Add initial concourse config

--- .ci/tasks/windows-test.bat
+++ .ci/tasks/windows-test.bat
@@ -1,12 +1,12 @@
 echo "Updating rust..."
-rustup update
+rustup update || exit /b 1
 
 echo "Adding SDL..."
-copy C:\SDL2-2.0.8\lib\x64\*.lib %USERPROFILE%\.multirust\toolchains\%RUST_CHANNEL%-%RUST_TARGET%\lib\rustlib\%RUST_TARGET%\lib
-copy C:\SDL2-2.0.8\lib\x64\*.dll .
+copy /Y C:\SDL2-2.0.8\lib\x64\*.lib %USERPROFILE%\.multirust\toolchains\%RUST_CHANNEL%-%RUST_TARGET%\lib\rustlib\%RUST_TARGET%\lib || exit /b 1
+copy /Y C:\SDL2-2.0.8\lib\x64\*.dll || exit /b 1
 
 echo "Running tests..."
-$env:RUSTFLAGS = "-D warnings"
-cargo +%RUST_CHANNEL% test --all
-cargo +%RUST_CHANNEL% test --all --features profiler
-cargo +%RUST_CHANNEL% test --all --features sdl_controller
+set RUSTFLAGS=-D warnings || exit /b 1
+cargo +%RUST_CHANNEL% test --all || exit /b 1
+cargo +%RUST_CHANNEL% test --all --features profiler || exit /b 1
+cargo +%RUST_CHANNEL% test --all --features sdl_controller || exit /b 1
