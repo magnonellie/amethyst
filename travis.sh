5d573ef8c9d6048e2f1e565c0f24be819bca3c6e
Ellie <hi@ellie.moe>
2018-10-16 14:08:36 +0200

    Change Travis config

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@ export RUSTFLAGS="-D warnings"
 echo "Build and test"
 cargo test --all || exit 1
 
-if [ $CHANNEL = "stable" ]
+if [ ${CHANNEL} = "stable" ]
 then
     echo "Build and test with sdl_controller"
     cargo check --all --features sdl_controller,profiler
